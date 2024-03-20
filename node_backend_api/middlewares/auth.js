53dfebd93a2e748f9b3360cc22911e1372cf122f
TurtleTipper78 <fabien.turgeon@gmail.com>
2024-03-20 14:58:35 -0400

    middleware

--- node_backend_api/middlewares/auth.js
+++ node_backend_api/middlewares/auth.js
@@ -1,32 +1,41 @@
 const jwt = require("jsonwebtoken");
-const db = require("../config/db.js")
+const db = require("../config/db");
 
-const auth = async(req,res,next)=>{
-    try {//si le jeton est valide
-        if(req.headers.authorization){
-            const jetonAValier = req.headers.authorization.split(" ")[1];
-            const jetonDecode = jwt.verify(jetonAValier,process.env.JWT_SECRET);
+const auth = async (req, res, next) => {
+    try {
+        //Si le jeton est valide
+        // Est-ce qu'il y quelque chose dans l'entête
+        if (req.headers.authorization) {
+            //Transforme en array et retourne la portion après Bearer
+            //Exemple d'auth: "Bearer 13wkjsd.hfwuisudhfiusdhf.uisdhf3"
+            const jetonAValider = req.headers.authorization.split(" ")[1];
+            const jetonDecode = jwt.verify(jetonAValider, process.env.JWT_SECRET);
 
-            const utilisateurVerifie = await db.collection("utilisateur").doc(jetonDecode.id)
+            const utilisateurVerifie = await db.collection("utilisateurs").doc(jetonDecode.id).get();
 
-            if(utilisateurVerifie.exist){
+            if (utilisateurVerifie.exists) {
+                //Si l'utilisateur existe, on permet la suite de la requête initiale
                 const utilisateurRecupere = utilisateurVerifie.data();
                 req.utilisateur = utilisateurRecupere;
 
-                // Appelle la suite de la requete initial
+                // Appelle la suite de la requête initiale.
                 next();
             } else {
-                // si utilisateur existe pas, on retourne une erreur non auth
-                throw new Error("non autorisé")
+                //Si l'utilisateur n'existe pas, on retourne une erreur non autorisée
+                // res.statusCode = 401;
+                // res.json({ message: "Non autorisé" });
+                throw new Error("Non autorisé");
             }
-            
         } else {
-            throw new Error("non autorisé")
+            // res.statusCode = 401;
+            // res.json({ message: "Non autorisé" });
+            throw new Error("Non autorisé");
         }
-    }catch(erreur){
+    } catch (erreur) {
+        console.log(erreur);
         res.statusCode = 401;
-        res.json({ message: erreur.message })
+        res.json({ message: erreur.message });
     }
-}
+};
 
 module.exports = auth;
